529d43986409c1f2d4e9d5f8878bd3441fc4498a
ABEL K ANTONY <abelkantony98@gmail.com>
2023-03-14 19:10:34 +0530

    update  to spawn hexagon

---
+++ Assets/Script/SpwanerScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanerScript : MonoBehaviour
+{
+
+    public float spawnTimePeriod = 1f;
+
+    public GameObject hexagonPrefab;
+
+    public float nextTimeToSpawn = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
