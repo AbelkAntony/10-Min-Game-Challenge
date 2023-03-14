68fd64b14e24c4f19d2a26546e0fdb7e7d561e42
ABEL K ANTONY <abelkantony98@gmail.com>
2023-03-14 19:53:29 +0530

    completed

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 600f;
@@ -16,6 +16,11 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * moveSpeed);
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Script/SpwanerScript.cs
+++ Assets/Script/SpwanerScript.cs
@@ -10,15 +10,14 @@ public class SpwanerScript : MonoBehaviour
     public GameObject hexagonPrefab;
 
     public float nextTimeToSpawn = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+            nextTimeToSpawn = Time.time + 1f / spawnTimePeriod;
+        }
     }
 }
